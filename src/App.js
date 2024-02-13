06f6d93dea41f4d894b6840be16901a2f569fbc7
Abdoulaye Bagayoko <155672513+Baga-Tim@users.noreply.github.com>
2024-02-13 14:27:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,3 +45,8 @@ const App = () => {
     );
 };
 export default App;
+
+. // Budget component
+                    . <div className='col-sm'>
+                      .   <Budget />
+                   .  </div>
